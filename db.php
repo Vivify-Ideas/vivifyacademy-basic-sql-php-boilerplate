d245bb682f1685d6b1cf08cc94b1b5562c1c4e10
Aleksandar Sotirovic <aleksandar.sotirovic@gmail.com>
2017-07-27 18:12:44 +0200

    example code

--- db.php
+++ db.php
@@ -13,4 +13,27 @@
     {
         echo $e->getMessage();
     }
+
+
+
+// pripremamo upit
+$sql = "SELECT id, title, created_at, content, created_by FROM posts ORDER BY created_at DESC LIMIT 3";
+$statement = $connection->prepare($sql);
+
+// izvrsavamo upit
+$statement->execute();
+
+// zelimo da se rezultat vrati kao asocijativni niz.
+// ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
+$statement->setFetchMode(PDO::FETCH_ASSOC);
+
+// punimo promenjivu sa rezultatom upita
+$posts = $statement->fetchAll();
+
+// koristite var_dump kada god treba da proverite sadrzaj neke promenjive
+//    echo '<pre>';
+                //    var_dump($posts);
+                //    echo '</pre>';
+
+
 ?>
