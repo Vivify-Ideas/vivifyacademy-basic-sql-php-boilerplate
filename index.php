3cfa2c8bc286bad7330bf15f733f26e2078564d5
Aleksandar Sotirovic <aleksandar.sotirovic@gmail.com>
2017-07-30 20:40:41 +0200

    starter for school

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+    // ako su mysql username/password i ime baze na vasim racunarima drugaciji
+    // obavezno ih ovde zamenite
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "root";
+    $dbname = "vivify_blog_3_dan";
+
+    try {
+        $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,38 +36,54 @@
     <div class="va-l-container">
         <main class="va-l-page-content">
 
-                <article class="va-c-article">
-                    <header>
-                        <h1><a href="single-post.php">Post title</a></h1>
-                        <div class="va-c-article__meta">12. 6. 2017. by John Doe</div>
-                    </header>
+            <?php
 
-                    <div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt vitae molestias rem repellendus commodi provident? Magnam, nobis quisquam perferendis consectetur deserunt laboriosam pariatur a, eum suscipit ratione iusto ullam aperiam quas quod culpa dolore corrupti voluptatem placeat enim commodi in.</p>
-                    </div>
-                </article>
+                // pripremamo upit
+                $sql = "SELECT id, title, created_at, content, created_by FROM posts ORDER BY created_at DESC LIMIT 3";
+                $statement = $connection->prepare($sql);
 
-            <article class="va-c-article">
-                <header>
-                    <h1><a href="single-post.php">Post title</a></h1>
-                    <div class="va-c-article__meta">12. 6. 2017. by John Doe</div>
-                </header>
+                // izvrsavamo upit
+                $statement->execute();
 
-                <div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt vitae molestias rem repellendus commodi provident? Magnam, nobis quisquam perferendis consectetur deserunt laboriosam pariatur a, eum suscipit ratione iusto ullam aperiam quas quod culpa dolore corrupti voluptatem placeat enim commodi in.</p>
-                </div>
-            </article>
+                // zelimo da se rezultat vrati kao asocijativni niz.
+                // ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
+                $statement->setFetchMode(PDO::FETCH_ASSOC);
 
-            <article class="va-c-article">
-                <header>
-                    <h1><a href="single-post.php">Post title</a></h1>
-                    <div class="va-c-article__meta">12. 6. 2017. by John Doe</div>
-                </header>
+                // punimo promenjivu sa rezultatom upita
+                $posts = $statement->fetchAll();
 
-                <div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt vitae molestias rem repellendus commodi provident? Magnam, nobis quisquam perferendis consectetur deserunt laboriosam pariatur a, eum suscipit ratione iusto ullam aperiam quas quod culpa dolore corrupti voluptatem placeat enim commodi in.</p>
-                </div>
-            </article>
+                // koristite var_dump kada god treba da proverite sadrzaj neke promenjive
+                    echo '<pre>';
+                    var_dump($posts);
+                    echo '</pre>';
+
+            ?>
+
+            <?php
+                foreach ($posts as $post) {
+            ?>
+
+                    <article class="va-c-article">
+                        <header>
+                            <h1><a href="single-post.php?post_id=<?php echo($post['id']) ?>"><?php echo($post['title']) ?></a></h1>
+
+                            <!-- zameniti  datum i ime sa pravim imenom i datumom blog post-a iz baze -->
+                            <div class="va-c-article__meta">12. 6. 2017. by John Doe</div>
+                        </header>
+
+                        <div>
+                            <!-- zameniti ovaj privremeni (testni) text sa pravim sadrzajem blog post-a iz baze -->
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt vitae molestias rem
+                                repellendus commodi provident? Magnam, nobis quisquam perferendis consectetur deserunt
+                                laboriosam pariatur a, eum suscipit ratione iusto ullam aperiam quas quod culpa dolore
+                                corrupti voluptatem placeat enim commodi in.</p>
+                        </div>
+                    </article>
+
+            <?php
+                }
+            ?>
+
 
             <div class="va-c-paginator">
                 <a href="all-posts.php" title="All posts">All posts</a>
